14bd6f0b8e7aad3247b3ae275983ad873331d575
yja90 <74431763+yja90@users.noreply.github.com>
2021-04-02 16:42:31 +0100

    Update dom.js

--- dom.js
+++ dom.js
@@ -2,8 +2,17 @@
 let playerScore = 0;
 let compScore = 0;
 let playerSelection;
+const instructions = document.querySelector('#instructions')
+const gameScoreP = document.querySelector('#gameScoreP')
+const gameScoreC = document.querySelector('#gameScoreC')
+const result = document.querySelector('#result')
+const resultOfRound = document.querySelector('#resultOfRound')
+const resultOfRound2 = document.querySelector('#resultOfRound2')
+const compsSelection = document.querySelector('#compsSelection')
+const playersSelection = document.querySelector('#playersSelection')
 
 
+//Function for computer to make a random choice
 function computerPlay() {
   let computerChoice = ["Rock", "Paper", "Scissors"]
   let randomChoice = Math.floor(Math.random() * computerChoice.length);
@@ -11,58 +20,56 @@ function computerPlay() {
 }
 
 
-
+//Plays game and decides on winner
 function playRound(playerSelection) {
-  const gameScoreP = document.querySelector('#gameScoreP')
-  const gameScoreC = document.querySelector('#gameScoreC')
-  const result = document.querySelector('#result')
-  const sameSelection = document.querySelector('#sameSelection')
-  const compsSelection = document.querySelector('#compsSelection')
-  const playersSelection = document.querySelector('#playersSelection')
   let computerSelection = computerPlay()
-
-
   if (playerSelection == "Rock" && computerSelection === "Scissors") {
     playerScore++
     gameScoreP.style.color = '#00FF00'
     gameScoreC.style.color = null
-    sameSelection.textContent = null
+    resultOfRound.textContent = 'Your round'
+    resultOfRound2.textContent = 'Rock beats scissors!'
     compsSelection.src = "images/scissors.png"
     playersSelection.src = "images/rock.png"
     resultContent()
   } else if (playerSelection == "Rock" && computerSelection == "Paper") {
     compScore++
-    gameScoreC.style.color = '#8B0000'
+    gameScoreC.style.color = '#00FF00'
     gameScoreP.style.color = null
-    sameSelection.textContent = null
+    resultOfRound.textContent = "Opponent's round"
+    resultOfRound2.textContent = 'Paper beats rock!'
     playersSelection.src = "images/rock.png"
     compsSelection.src = "images/paper.png"
     resultContent()
   } else if (playerSelection == "Rock" && computerSelection == "Rock") {
     gameScoreP.style.color = null
     gameScoreC.style.color = null
-    sameSelection.textContent = 'You both chose rock, please make another selection'
+    resultOfRound.textContent = 'Bad luck, it was a draw. Go again!'
+    resultOfRound2.textContent = null
     playersSelection.src = "images/rock.png"
     compsSelection.src = "images/rock.png"
     resultContent()
   } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
     compScore++
-    sameSelection.textContent = null
-    gameScoreC.style.color = '#8B0000'
+    resultOfRound.textContent = "Opponent's round"
+    resultOfRound2.textContent = 'Rock beats scissors!'
+    gameScoreC.style.color = '#00FF00'
     gameScoreP.style.color = null
     compsSelection.src = "images/rock.png"
     playersSelection.src = "images/scissors.png"
     resultContent()
   } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
     playerScore++
-    sameSelection.textContent = null
+    resultOfRound.textContent = 'Your round'
+    resultOfRound2.textContent = 'Scissors beats paper!'
     gameScoreP.style.color = '#00FF00'
     gameScoreC.style.color = null
     compsSelection.src = "images/paper.png"
     playersSelection.src = "images/scissors.png"
     resultContent()
   } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
-    sameSelection.textContent = 'You both chose scissors, please make another selection'
+    resultOfRound.textContent = 'Bad luck, it was a draw. Go again!'
+    resultOfRound2.textContent = null
     gameScoreP.style.color = null
     gameScoreC.style.color = null
     compsSelection.src = "images/scissors.png"
@@ -70,58 +77,65 @@ function playRound(playerSelection) {
     resultContent()
   } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
     compScore++;
-    sameSelection.textContent = null
+    resultOfRound.textContent = "Opponent's round"
+    resultOfRound2.textContent = 'Scissors beats paper!'
     gameScoreP.style.color = null
-    gameScoreC.style.color = '#8B0000'
+    gameScoreC.style.color = '#00FF00'
     compsSelection.src = "images/scissors.png"
     playersSelection.src = "images/paper.png"
     resultContent()
   } else if (playerSelection == "Paper" && computerSelection == "Rock") {
     playerScore++;
-    sameSelection.textContent = null
+    resultOfRound.textContent = "Your round"
+    resultOfRound2.textContent = 'Paper beats rock!'
     gameScoreP.style.color = '#00FF00'
     gameScoreC.style.color = null
     compsSelection.src = "images/rock.png"
     playersSelection.src = "images/paper.png"
     resultContent()
   } else if (playerSelection == "Paper" && computerSelection == "Paper") {
-    sameSelection.textContent = 'You both chose paper, please make another selection'
+    resultOfRound.textContent = "Bad luck, it was a draw. Go again!"
+    resultOfRound2.textContent = null
     gameScoreP.style.color = null
     gameScoreC.style.color = null
     compsSelection.src = "images/paper.png"
     playersSelection.src = "images/paper.png"
     resultContent()
   }
-
-  function resultContent() {
-    gameScoreP.textContent = playerScore
-    gameScoreC.textContent = compScore
-  }
   if (playerScore === 5) {
     result.textContent = 'You win!';
+    resetButton.style.top = '45%'
     disableContent()
   } else if (compScore === 5) {
     result.textContent = 'Computer wins!'
     disableContent()
+    resetButton.style.top = '45%'
   } else {
     result.textContent = null
   }
+  instructions.textContent = null
 }
 
+//Displays player and computer score
+function resultContent() {
+  gameScoreP.textContent = playerScore
+  gameScoreC.textContent = compScore
+}
 
-
+//Disables buttons and removes content
 function disableContent() {
   rock.disabled = true;
   scissors.disabled = true;
   paper.disabled = true;
   playerScore = 0;
   compScore = 0;
-  compsSelection.src = null
-  playersSelection.src = null
+  resultOfRound.textContent = null
+  resultOfRound2.textContent = null
+  instructions.textContent = null
 }
 
 
-
+// Resets all values
 function resetValues() {
   rock.disabled = false;
   scissors.disabled = false
@@ -131,12 +145,14 @@ function resetValues() {
   result.textContent = null
   gameScoreP.textContent = "0"
   gameScoreC.textContent = "0"
-  compsSelection.textContent = 'Computer chose:'
   gameScoreP.style.color = null
   gameScoreC.style.color = null
-  sameSelection.textContent = null
+  resultOfRound.textContent = null
+  resultOfRound2.textContent = null
   playersSelection.src = null
   compsSelection.src = null
+  instructions.textContent = 'Start the game by making a selection'
+  resetButton.style.top = '85%'
 }
 
 
@@ -154,8 +170,6 @@ const paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
   playRound("Paper")
 })
-
-
 
 const resetButton = document.querySelector('#resetButton');
 resetButton.addEventListener('click', resetValues);
